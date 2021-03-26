03c58f19aa1ffb68104a35850157bd61757362a3
Melodiean <melodynsita@gmail.com>
2021-03-26 04:06:48 +0300

    Edit form functionality

--- contact.php
+++ contact.php
@@ -1,38 +1,22 @@
-<!-- <?php print $_POST['guest_email']; ?> -->
-
-<!-- Thanks for your message! <?php print $_POST['guest_message'] ?> -->
-
 <?php
-error_reporting(-1);
-ini_set('display_errors', 'On');
-set_error_handler("var_dump");
+// error_reporting(-1);
+// ini_set('display_errors', 'On');
+// set_error_handler("var_dump");
 
-$name = $_POST["guest_name"];
-$email = $_POST["guest_email"];
-$subject = $_POST["mail_title"];
-$content = $_POST["guest_message"];
+if (isset($_POST["guest_email"])) {
 
-$to = "adieuxtah@gmail.com";
-
-$headers = array("From: adieuxtah@gmail.com",
-    "Reply-To: adieuxtah@gmail.com",
-    "MIME-Version: 1.0",
-    "Content-Type: text/html; charset=ISO-8859-1",
-    "X-Priority: 1",
-    "X-Mailer: PHP/" . PHP_VERSION
-);
-// $subject = "Email from my website";
-// $body = "the user typed in ...";
-$status = mail('adieuxtah@gmail.com', $subject,$content,$headers);
-
-echo $status;
+    $name = $_POST["guest_name"];
+    $email = $_POST["guest_email"];
+    $subject = $_POST["mail_title"];
+    $content = $_POST["guest_message"];
 
 
-if($status)
-{
+    $headers  = 'From: ' . $email . "\n";
+    $headers .= 'Reply-to: ' . $email . "\n";
+
+    mail('adieuxtah@gmail.com', $subject, $content, $headers);
+
     echo '<p>Your mail has been sent!</p>';
 } else {
     echo '<p>Something went wrong. Please try again!</p>';
 }
-echo $status;
-?>
